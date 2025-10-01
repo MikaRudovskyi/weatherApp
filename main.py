3815df72568da0b61d0614d8184806df08c78596
Mykhailo Rudovskyi <mikhail.rudovsky@gmail.com>
2025-10-01 20:45:11 +0200

    Remove unnecessary comments from main.py for clarity

--- main.py
+++ main.py
@@ -10,10 +10,8 @@ if __name__ == "__main__":
     splash = SplashScreen()
     splash.show()
 
-    # Створюємо головне вікно, але не показуємо його одразу
     weather_app = WeatherApp()
 
-    # Даємо заставці показатися 3 секунди, а потім закриваємо її і показуємо головне вікно
     QTimer.singleShot(3000, lambda: (splash.close(), weather_app.show()))
 
     sys.exit(app.exec_())
